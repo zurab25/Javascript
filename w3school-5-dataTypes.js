e4051eff63aaa05cb7736b854388e6952c82131b
meskhidze25 <zurab.meskhidze@gmail.com>
2018-12-02 13:56:50 +0400

    Javascript Examples

---
+++ w3school-5-dataTypes.js
@@ -0,0 +1,7 @@
+function demoo(){
+    var pi=3.14;
+    var person= "John doe";
+    var answer="I am here";
+
+    document.getElementById("demo").innerHTML = pi + "<br>" + person + "<br>" + answer;
+}
