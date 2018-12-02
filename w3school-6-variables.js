28d8d7f6ad649173619a320ce6c35ccd637bf03a
meskhidze25 <zurab.meskhidze@gmail.com>
2018-12-02 15:20:37 +0400

    New Exapmles of JavaScript

---
+++ w3school-6-variables.js
@@ -0,0 +1,11 @@
+function MultiVariables(){
+    var person = "Zurab Meskhidze", carName="BMW", price="200";
+
+    document.getElementById("demos").innerHTML = carName;
+}
+
+function Arithmetics(){
+    var x = 2+5+9;
+
+    document.getElementById("aritmetics").innerHTML = x;
+}
