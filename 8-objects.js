1e8ab34ca9c0817670eeffdf334f17156847a7cf
meskhidze25 <zurab.meskhidze@gmail.com>
2019-04-03 10:06:03 +0400

    Javascript Examples

---
+++ 8-objects.js
@@ -0,0 +1,8 @@
+//create object:
+function myObjects(){
+    var car = {type:"Fiat", model:"500",color:"white"};
+
+    //Display some data from the obect
+
+    document.getElementById("demo").innerHTML = "The car type is " + car.type;
+}
